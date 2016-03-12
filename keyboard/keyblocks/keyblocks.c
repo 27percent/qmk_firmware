1027df10940725384eb1e19c38f100425c045db1
Josh Pickett <josh@27percent.com>
2016-03-12 19:20:52 +0000

    merging

--- keyboard/keyblocks/keyblocks.c
+++ keyboard/keyblocks/keyblocks.c
@@ -24,13 +24,13 @@ void * matrix_init_kb(void) {
     DDRB  &= ~(1<<4);  // set B(4) as input
     PORTB &= ~(1<<4);  // set B(4) internal pull-up disabled
 
-    // unused pins - C7, D4, D5, D7, E6
+    // unused pins - D7, E6
     // set as input with internal pull-ip enabled
-    DDRC  &= ~(1<<7);
+    // DDRC  &= ~(1<<7);
     DDRD  &= ~(1<<7);
     DDRE  &= ~(1<<6);
-    PORTC |=  (1<<7);
-    PORTD |=  (1<<7 | 1<<5 | 1<<4);
+    // PORTC |=  (1<<7);
+    PORTD |=  (1<<7);
     PORTE |=  (1<<6);
 
     if (matrix_init_user) {
